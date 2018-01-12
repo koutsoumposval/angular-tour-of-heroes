8ddfc1c03ae9fc0d4f3668cbeb472ee5b7ae356a
Valantis Koutsoumpos <chrysovalantis.koutsoumpos@devmob.com>
2018-01-12 16:43:04 +0100

    Initial Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Tijmen' },
+  { id: 12, name: 'Maarten' },
+  { id: 13, name: 'Daan' },
+  { id: 14, name: 'Koen' },
+  { id: 15, name: 'Lachezar' },
+  { id: 16, name: 'Val' },
+  { id: 17, name: 'Thao' },
+  { id: 18, name: 'Danniele' },
+  { id: 19, name: 'Alissa' },
+  { id: 20, name: 'Gerard' }
+];
